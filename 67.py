4633a1003f308cbd4ca5d3d9ff270324931fde99
Dark-sid3 <darkside29236@gmail.com>
2026-05-07 18:57:37 +0600

    67.py

---
+++ 67.py
@@ -0,0 +1,13 @@
+Siix = input
+seveen = print
+
+six = int(siix("Введите число:"))
+seven = int(siix("Введите число:"))
+
+seveen(six + seven)
+seveen(six - seven)
+seveen(six * seven)
+seveen(six // seven)
+
+sixx = "he"
+seveen(sixx * 100)
