3dd8cb5c2a84b796c750e7e60633f2b62f63d20c
Dark-sid3 <darkside29236@gmail.com>
2026-05-08 22:48:42 +0600

    if-else.py

---
+++ if-else.py
@@ -0,0 +1,7 @@
+emotion = input('Приветик! Вы сейчас счастливы?')
+if emotion.capitalize() == 'Да':
+	input('Оо, славно! Расскажете, почему?')
+elif emotion.capitalize() == 'Нет':
+	input('Грустненько, расскажете почему?')
+else :
+	input('Ого, как так?')
